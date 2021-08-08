dbd8f083b436ba11e9c282ca2c4696b41d2f239f
miki <milanjecmenica@gmail.com>
2021-08-08 16:12:04 +0200

    hehe

--- jenkins/mvn.sh
+++ jenkins/mvn.sh
@@ -3,5 +3,6 @@
 echo $WORKSPACE
 echo $HOME
 
-docker run --rm -u root -v "$WORKSPACE":/usr/src/app -v "$HOME/.m2":/root/.m2 -w /usr/src/app maven ls
-docker run --rm -u root -v "$WORKSPACE":/usr/src/app -v "$HOME/.m2":/root/.m2 -w /usr/src/app maven mvn $@
+docker run --rm -u root -v $WORKSPACE:/usr/src/app -v $HOME/.m2:/root/.m2 -w /usr/src/app maven echo 'hehe'
+docker run --rm -u root -v $WORKSPACE:/usr/src/app -v $HOME/.m2:/root/.m2 -w /usr/src/app maven ls
+docker run --rm -u root -v $WORKSPACE:/usr/src/app -v $HOME/.m2:/root/.m2 -w /usr/src/app maven mvn $@
