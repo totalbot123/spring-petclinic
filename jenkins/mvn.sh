85fbb5e75f1e49179530bc83c4db984158153680
miki <milanjecmenica@gmail.com>
2021-08-08 16:05:00 +0200

    hehe

--- jenkins/mvn.sh
+++ jenkins/mvn.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+echo $WORKSPACE
+echo $HOME
+ls -ltra $HOME
+ls -ltra $WORKSPACE
 docker run --rm -u root -v "$WORKSPACE":/usr/src/app -v "$HOME/.m2":/root/.m2 -w /usr/src/app maven mvn $@
