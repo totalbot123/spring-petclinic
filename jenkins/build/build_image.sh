d0ff8d6c29600791fa52e28c3ca934f8a195a118
miki <milanjecmenica@gmail.com>
2021-08-11 14:49:26 +0200

    hehe

--- jenkins/build/build_image.sh
+++ jenkins/build/build_image.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-IMAGE_NAME=$1
-IMAGE_TAG=$2
+cp -f target/*jar jenkins/build
 
-docker build --build-arg WORKSPACE=$WORKSPACE ./jenkins/build -t $IMAGE_NAME:$IMAGE_TAG
+docker build --no-cache ./jenkins/build -t $IMAGE_NAME:$IMAGE_TAG 
